ec8d585bf96cb29578462910b1d0e53b7f554b38
João Álex Marques Antunes <joaoalex.marques2023@gmail.com>
2026-08-15 14:56:14 -0300

    feat: complete Python student profile exercise

---
+++ fase-1/python/basico/exercicio-01/main.py
@@ -0,0 +1,7 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+course = input("Enter your course: ")
+semester = int(input("Enter your semester: "))
+
+
+print(f"===Student Profile===\nHello, {name}! \nYou are {age} years old, \nstudying {course} \nin semester {semester}.")
